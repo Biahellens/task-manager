13b8b85e1fb74b3ebac26a9517ee9d5afd600e92
biahellens <60991740+Biahellens@users.noreply.github.com>
2024-01-11 14:08:28 -0300

    feat: adiconando page de tarefas

---
+++ app/src/store/index.ts
@@ -0,0 +1,5 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { createPinia } from 'pinia';
+
+// eslint-disable-next-line import/prefer-default-export
+export const pinia = createPinia();
---
+++ app/src/views/TasksView.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="main">
+    <div class="navBarContent">
+      <NavBar />
+    </div>
+    <div class="content">
+      <div class="topContent">
+        <div class="boxsContent">
+          <BoxNewTask />
+          <BoxStatusTasks />
+        </div>
+      </div>
+      <div class="tableContent">
+        <TableTask />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+// eslint-disable-next-line import/no-unresolved
+import TableTask from '../components/TableTask.vue';
+import NavBar from '../components/VerticalBar.vue';
+import BoxNewTask from '../components/BoxNewTask.vue';
+import BoxStatusTasks from '../components/BoxStatusTasks.vue';
+
+@Options({
+  components: {
+    TableTask,
+    NavBar,
+    BoxNewTask,
+    BoxStatusTasks,
+  },
+})
+export default class Tasks extends Vue {}
+</script>
+
+<style scoped>
+  .main {
+    width: 100%;
+    height: auto;
+    display: flex;
+    margin: 0;
+    padding: 0;
+
+    @media (max-width: 768px) {
+      flex-flow: column;
+    }
+  }
+
+  .navBarContent {
+    width: 20%;
+
+    @media (max-width: 768px) {
+      width: 100%;
+    }
+  }
+
+  .content {
+    width: 80%;
+
+    @media (max-width: 768px) {
+      width: 100%;
+    }
+  }
+
+  .tableContent {
+    display: flex;
+    justify-content: center;
+  }
+
+  .topContent {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+
+  .boxsContent {
+    width: 80%;
+    display: flex;
+    justify-content: space-between;
+    margin: 80px 0 40px 0;
+
+    @media (max-width: 768px) {
+      width: 90%;
+      display: flex;
+      justify-content: space-between;
+    }
+  }
+
+</style>
