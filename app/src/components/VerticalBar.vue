fc60179ddcd5cf6b19ccd78c6c48ee122870114f
biahellens <60991740+Biahellens@users.noreply.github.com>
2024-01-11 14:07:25 -0300

    feat: adicionando header lateral

---
+++ app/src/components/VerticalBar.vue
@@ -0,0 +1,120 @@
+<!-- eslint-disable vuejs-accessibility/mouse-events-have-key-events -->
+<template>
+  <div class="navbar">
+    <div class="logoItems">
+      <img class="logoImg" alt="logo" src="../assets/logo.svg" />
+      <h3>Task-manager</h3>
+    </div>
+    <div class="logoutItems" @mouseover="hover = true" @mouseleave="hover = false">
+      <button class="buttonLogout" @click="logout">Sair da conta</button>
+      <img class="logout" alt="logout" src="../assets/logout.svg" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+// eslint-disable-next-line import/extensions
+import { useVerticalBarStore } from '../store/modules/verticalBarStore';
+
+export default class verticalBar extends Vue {
+  private verticalBarStore = useVerticalBarStore();
+
+  private get hover() {
+    return this.verticalBarStore.hover;
+  }
+
+  private set hover(value: boolean) {
+    this.verticalBarStore.hover = value;
+  }
+
+  private logout() {
+    this.verticalBarStore.logout(this.$router);
+  }
+}
+</script>
+
+<style scoped>
+.navbar {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-wrap: wrap;
+  align-content: space-between;
+  background-color: #f0f0f0;
+  color: #4b0082;
+
+  @media (max-width: 768px) {
+    height: 80px;
+    flex-wrap: nowrap;
+  }
+}
+
+.logoItems {
+  width: 100%;
+  height: 30px;
+  padding: 8px;
+  display: flex;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    width: 50%;
+  }
+}
+
+.logoImg {
+  width: 30px;
+  height: 30px;
+}
+
+h3 {
+  text-align: center;
+  margin: 0;
+}
+
+.logoutItems {
+  width: 100%;
+  height: 30px;
+  padding: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: end;
+
+  @media (max-width: 768px) {
+    width: 50%;
+    padding-top: 16px;
+    justify-content: end;
+  }
+}
+
+p {
+  font-size: 16px;
+  font-weight: 600;
+  margin: 0 8px 0 0;
+
+  @media (max-width: 768px) {
+    font-size: 12px;
+    text-align: center;
+    line-height: 20px;
+  }
+}
+
+.logout {
+  width: 20px;
+  height: 20px;
+}
+
+.buttonLogout {
+  background-color: transparent;
+  border: none;
+  color: #4b0082;
+  font-size: 14px;
+  font-weight: 600;
+  cursor: pointer;
+}
+
+.logoutItems:hover {
+  transform: scale(1.1);
+}
+</style>
+../store/modules/modules/verticalBarStore
---
+++ app/src/store/modules/verticalBarStore.ts
@@ -0,0 +1,46 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineStore } from 'pinia';
+import { Router } from 'vue-router';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import axios from 'axios';
+
+// eslint-disable-next-line import/prefer-default-export
+export const useVerticalBarStore = defineStore('verticalBar', {
+  state: () => ({
+    hover: false,
+  }),
+  actions: {
+    logout(router: Router) {
+      function setCookie(name: string, value: string, days: number) {
+        const expires = new Date();
+        expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+        document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+      }
+
+      function getCookie(name: string) {
+        // eslint-disable-next-line prefer-template
+        const cookieValue = document.cookie.match('(^|[^;]+)\\s*' + name + '\\s*=\\s*([^;]+)');
+        return cookieValue ? cookieValue.pop() : '';
+      }
+
+      const authToken = getCookie('token');
+
+      if (!authToken) {
+        console.error('Token de autenticação não disponível');
+        return;
+      }
+
+      axios.post('http://localhost:3000/users/logout', null, {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      }).then(() => {
+        setCookie('token', '', -1);
+        localStorage.removeItem('token');
+        router.push('/login');
+      }).catch((error) => {
+        console.error('Erro no logout:', error.response.data.message);
+      });
+    },
+  },
+});
